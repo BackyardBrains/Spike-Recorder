cdb88319c68ecbd6b0c33ce2dd07cb05009f173e
laochailan <laochailan@web.de>
2014-03-05 15:06:26 +0100

    mac dialog fix?

--- src/widgets/native/FileDialogMac.cpp
+++ src/widgets/native/FileDialogMac.cpp
@@ -8,24 +8,20 @@ namespace BackyardBrains {
 namespace Widgets {
 
 
-void *FileDialog::startThread(void *arg) {
-	reinterpret_cast<FileDialog *>(arg)->openDialog();
-	return 0;
-}
-
-void FileDialog::openDialog() {
-	_state = (DialogState)FileDialogMac::openFileDialog(&_fileName);
-	_opened = false;
-}
+// void *FileDialog::startThread(void *arg) {
+// 	reinterpret_cast<FileDialog *>(arg)->openDialog();
+// 	return 0;
+// }
+//
+// void FileDialog::openDialog() {
+// 	_state = (DialogState)FileDialogMac::openFileDialog(&_fileName);
+// 	_opened = false;
+// }
 
 int FileDialog::open() {
 	pthread_t thread;
 
-	if(_opened)
-		return -1;
-
-	_opened = true;
-	int rc = pthread_create(&thread, NULL, &FileDialog::startThread, this);
+	_state = (DialogState)FileDialogMac::openFileDialog(&_fileName);
 
 	return !rc;
 }
