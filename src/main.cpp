dab67c8be74ffd03d5c14d4b21592db8160a4008
laochailan <laochailan@web.de>
2014-03-27 11:00:24 +0100

    change working dir to executable directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include "widgets/Application.h"
-// #include "widgets/Widget.h"
 #include "Game.h"
 #include <SDL.h>
+#include <cstring>
+#include <string>
 
-int main(int argc, char *argv[])
-{
+#ifdef __WIN32__
+#include <direct.h>
+#else
+#include <unistd.h>
+#endif
+
+int main(int argc, char *argv[]) {
+	char *lastslash = strrchr(argv[0], '\n');
+	if(lastslash == NULL)
+		lastslash = argv[0]+strlen(argv[0]);
+
+	std::string path(argv[0], lastslash);
+	chdir(path.c_str());
+
 	BackyardBrains::Game game;
 	
 	game.run();
