c89e9a0b9bdedb9720eed7008d1f5ae366274799
laochailan <laochailan@web.de>
2014-04-13 10:53:25 +0200

    fixed ruler out of bounds bug

--- src/AudioView.cpp
+++ src/AudioView.cpp
@@ -616,7 +616,7 @@ void AudioView::mouseMotionEvent(Widgets::MouseEvent *event) {
 	}
 
 	if(_rulerStart != -1) {
-		_rulerEnd = event->pos().x;
+		_rulerEnd = std::max(event->pos().x, MOVEPIN_SIZE*3/2);
 		event->accept();
 	}
 
